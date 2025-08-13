72474daac968ca33306a1eac274782bbbd39cf19
elplug01 <elplug64@proton.me>
2025-08-12 20:42:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-const TelegramBot = require("node-telegram-bot-api");
-const celebs = require("./celebs.json");
+const TelegramBot = require('node-telegram-bot-api');
+const celebs = require('./celebs.json');
 
-const token = process.env.BOT_TOKEN;
+const token = process.env.BOT_TOKEN; // Make sure BOT_TOKEN is set in Railway Variables
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Welcome! Type /list to see all celebs.");
+  bot.sendMessage(msg.chat.id, 'Welcome! Type /list to see all celebs.');
 });
 
 bot.onText(/\/list/, (msg) => {
-  let reply = celebs.map((c, i) => `${i + 1}. ${c.name}`).join("\n");
-  bot.sendMessage(msg.chat.id, reply + "\n\nSend /celeb <number> to get details.");
+  let message = celebs.map((c, i) => `${i + 1}. ${c.name}`).join('\n');
+  message += '\n\nSend /celeb <number> to get details.';
+  bot.sendMessage(msg.chat.id, message);
 });
 
-bot.onText(/\/celeb (\d+)/, (msg, match) => {
-  const index = parseInt(match[1], 10) - 1;
+bot.onText(/\/celeb (.+)/, (msg, match) => {
+  const index = parseInt(match[1]) - 1;
   if (index >= 0 && index < celebs.length) {
     const celeb = celebs[index];
-    bot.sendPhoto(msg.chat.id, celeb.picture, {
-      caption: `${celeb.name}\nBio: ${celeb.bio}`
-    });
+    bot.sendMessage(msg.chat.id, `${celeb.name}\n${celeb.bio}\n${celeb.url}`);
   } else {
-    bot.sendMessage(msg.chat.id, "Invalid number.");
+    bot.sendMessage(msg.chat.id, 'Invalid number. Use /list to see options.');
   }
 });
